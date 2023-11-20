ea0b20aa7f860b01f165b224bd6a213257374c6a
Ben <146076866+Benfoxyy@users.noreply.github.com>
2023-11-20 20:25:50 +0330

    connect sqlite to contact page

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import contact
 
-# Register your models here.
+@admin.register(contact)
+class contactAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    list_display=('name','subject','email','created_date')
+    list_filter=['subject']
+    search_fields=['subject','message']
+    class Meta:
+        ordering=['-created_date']
