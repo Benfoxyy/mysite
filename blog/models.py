5e3078851cf41da4bbe3a39bbcb5eb6572838448
Ben <146076866+Benfoxyy@users.noreply.github.com>
2023-11-26 21:30:03 +0330

    static to dynamic
    changed static form of every post picture to dynamic

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
+from distutils.command import upload
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+    picture=models.ImageField(upload_to='blog/',default='blog/default.jpg')
+    auth=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title=models.CharField(max_length=50)
     content=models.TextField()
+    
     counted_views=models.IntegerField(default=0)
     status=models.BooleanField(default=False)
     published_date=models.DateTimeField(null=True)
@@ -19,4 +24,3 @@ class Post(models.Model):
     #category
     #tag
     #image
-    #auth
