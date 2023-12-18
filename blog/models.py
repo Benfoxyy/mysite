95b9ec079fcb09c6bf4ab012cda26872d4eba2c0
Ben <146076866+Benfoxyy@users.noreply.github.com>
2023-12-18 21:26:46 +0330

    sitemaps
    add blog sitemaps for blog

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 
 class category(models.Model):
@@ -25,3 +27,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     #tag
+
+    def get_absolute_url(self):
+        return reverse("blog:single", kwargs={"pid": self.id})
+    
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,12 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
-from website.sitemaps import StaticViewSitemap 
+from website.sitemaps import StaticViewSitemap
+from blog.sitemaps import BlogSitemap
 
 sitemaps = {
     'static': StaticViewSitemap,
+    'blog': BlogSitemap
 }
 
 urlpatterns = [
