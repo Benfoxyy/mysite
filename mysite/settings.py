52e95ca52200e6c96fc8a55acea79851c4ae609c
Ben <146076866+Benfoxyy@users.noreply.github.com>
2023-12-24 15:04:57 +0330

    multi captcha
    add multi captcha to admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +53,10 @@ SITE_ID = 3
 
 ROBOTS_USE_SITEMAP = False
 ROBOTS_USE_HOST = False
+
+MULTI_CAPTCHA_ADMIN = {
+    'engine': 'simple-captcha',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
