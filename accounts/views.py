9a6afa945a631d7c1d874d540fb2b6bea65fc30e
Ben <146076866+Benfoxyy@users.noreply.github.com>
2023-12-30 19:18:44 +0330

    checked for login or no

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 def login_view(request):
-    return render(request,'login.html')
+    if request.user.is_authenticated:
+        msg=f'{request.user.username} is logged in before!'
+    else:
+        msg='is not logged in!'
+    return render(request,'accounts/login.html',{'msg':msg})
 
 def logout_view(request):
-    return render(request,'logout.html')
+    return render(request,'accounts/logout.html')
 
 def signup_view(request):
-    return render(request,'signup.html')
+    return render(request,'accounts/signup.html')
