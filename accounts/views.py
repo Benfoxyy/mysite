d56a863867a720168abd6eb63fc056d1e13e3017
Ben <146076866+Benfoxyy@users.noreply.github.com>
2023-12-30 21:17:40 +0330

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
-    if request.user.is_authenticated:
-        msg=f'{request.user.username} is logged in before!'
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request,data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
     else:
-        msg='is not logged in!'
-    return render(request,'accounts/login.html',{'msg':msg})
+        return redirect('/')
+    form = AuthenticationForm()
+    return render(request,'accounts/login.html',{'form':form})
 
 def logout_view(request):
     return render(request,'accounts/logout.html')
